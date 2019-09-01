50adb38f48c921f72ebd171bba350c1e6df9fe8a
Martin Billinger <flkazemakase@gmail.com>
2019-09-01 10:14:56 +0200

    add comments

--- 04-side-effects/exercise4-02.scm
+++ 04-side-effects/exercise4-02.scm
@@ -2,6 +2,8 @@
         (libs utils)
         (libs book))
 
+; cons/car/cdr with only lambda terms
+
 (define (lcons a d) (lambda (selector) (selector a d)))
 (define (lcar pair) (pair (lambda (a d) a)))
 (define (lcdr pair) (pair (lambda (a d) d)))
