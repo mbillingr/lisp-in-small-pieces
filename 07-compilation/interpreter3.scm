f8e93b63a48a76d60b9f6acff18c0e54e7fe0c77
Martin Billinger <flkazemakase@gmail.com>
2019-09-24 18:38:05 +0200

    (wip) chapter 7 interpreter 3

--- 07-compilation/interpreter3.scm
+++ 07-compilation/interpreter3.scm
@@ -68,19 +68,92 @@
        (vector-set! instruction-size-table n (size-clause args))
        (vector-set! instruction-decode-table n (lambda (fetcher) (decode-clause fetcher name args)))))))
 
+(define (check-byte j)
+  (if (or (< j 0) (> j 255))
+      (static-wrong "Cannot pack this number within a byte" j)))
+
+(define (SHALLOW-ARGUMENT-REF j)
+  (check-byte j)
+  (case j
+    ((0 1 2 3) (list (+ 1 j)))
+    (else      (list 5 j))))
+
+(define-instruction (SHALLOW-ARGUMENT-REF0) 1
+  (set! *val* (activation-frame-argument *env* 0)))
+(define-instruction (SHALLOW-ARGUMENT-REF1) 2
+  (set! *val* (activation-frame-argument *env* 1)))
+(define-instruction (SHALLOW-ARGUMENT-REF2) 3
+  (set! *val* (activation-frame-argument *env* 2)))
+(define-instruction (SHALLOW-ARGUMENT-REF3) 4
+  (set! *val* (activation-frame-argument *env* 3)))
 (define-instruction (SHALLOW-ARGUMENT-REF j) 5
   (set! *val* (activation-frame-argument *env* j)))
 
-(define-instruction (DUMMY) 0
-  (println "DUMMY"))
+(define (SET-SHALLOW-ARGUMENT! j)
+  (case j
+    ((0 1 2 3) (list (+ 21 j)))
+    (else      (list 25 j))))
 
-(define-instruction (INT x) 1
-  (println x))
+(define-instruction (SET-SHALLOW-ARGUMENT!2) 21
+  (set-activation-frame-argument! *env* 0 *val*))
 
-(define *code* (make-vector 3))
-(vector-set! *code* 0 1)
-(vector-set! *code* 1 42)
-(vector-set! *code* 2 0)
-(define *pc* 0)
+(define-instruction (SET-SHALLOW-ARGUMENT!1) 22
+  (set-activation-frame-argument! *env* 1 *val*))
+
+(define-instruction (SET-SHALLOW-ARGUMENT!2) 23
+  (set-activation-frame-argument! *env* 2 *val*))
+
+(define-instruction (SET-SHALLOW-ARGUMENT!3) 24
+  (set-activation-frame-argument! *env* 3 *val*))
+
+(define-instruction (SET-SHALLOW-ARGUMENT! j) 25
+  (set-activation-frame-argument! *env* j *val*))
+
+(define (DEEP-ARGUMENT-REF i j) (list 6 i j))
+(define (SET-DEEP-ARGUMENT! i j) (list 26 i j))
+
+(define-instruction (DEEP-ARGUMENT-REF i j) 6
+  (set! *val* (deep-fetch *env* i j)))
+
+(define-instruction (SET-DEEP-ARGUMENT! i j) 26
+  (deep-update! *env* i j *val*))
+
+(define (GLOBAL-REF i) (list 7 i))
+(define (CHECKED-GLOBAL-REF i) (list 8 i))
+(define (SET-GLOBAL! i) (list 27 i))
+
+(define-instruction (GLOBAL-REF i) 7
+  (set! *val* (global-fetch i)))
+
+(define-instruction (CHECKED-GLOBAL-REF i) 8
+  (set! *val* (global-fetch i))
+  (if (eq? *val* undefined-value)
+      (signal-exception #t (list "Uninitialized global variable" i))))
+
+(define-instruction (SET-GLOBAL! i) 27
+  (global-update! i *val*))
+
+(define (PREDEFINED i)
+  (check-byte i)
+  (case i
+    ((0 1 2 3 4 5 6 7 8) (list (+ 10 i)))
+    (else                (list 19 i))))
+
+(define-instruction (PREDEFINED0) 10 (set! *val* #t))
+(define-instruction (PREDEFINED1) 11 (set! *val* #f))
+(define-instruction (PREDEFINED2) 12 (set! *val* '()))
+(define-instruction (PREDEFINED3) 13 (set! *val* cons))
+(define-instruction (PREDEFINED4) 14 (set! *val* car))
+(define-instruction (PREDEFINED5) 15 (set! *val* cdr))
+(define-instruction (PREDEFINED6) 16 (set! *val* pair?))
+(define-instruction (PREDEFINED7) 17 (set! *val* symbol?))
+(define-instruction (PREDEFINED8) 18 (set! *val* eq?))
+(define-instruction (PREDEFINED i) 19
+  (set! *val* (predefined-fetch i)))
+
+
 
 ; ===========================================================================
+
+(define (quotation-fetch i)
+  (vector-ref *constants* i))
