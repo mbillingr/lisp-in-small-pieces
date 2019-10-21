e852179c2a929d13f7b404114b2c6bc270c52436
Martin Billinger <mbillingr@users.noreply.github.com>
2019-10-21 07:47:17 +0200

    update reflective-interpreter

--- 08-reflection/reflective-interpreter.scm
+++ 08-reflection/reflective-interpreter.scm
@@ -7,27 +7,27 @@
                 (monitor (lambda (c b) (exit b))
                   ((lambda (it extend error global-env
                             toplevel eval evlis eprogn reference)
-                      '(set! extend
-                             (lambda (env names values)
-                               (if (pair? names)
-                                 (if (pair? values)
-                                     ((lambda (newenv)
-                                        (begin
-                                          (set-variable-value! (car names) newenv (car values))
-                                          (extend newenv (cdr names) (cdr values))))
-                                      (enrich env (car names)))
-                                     (error "Too few arguments" names))
-                                 (if (symbol? names)
-                                     ((lambda (newenv)
-                                        (begin
-                                          (set-variable-value! names newenv values)
-                                          newenv))
-                                      (enrich env names))
-                                     (if (null? names)
-                                         (if (null? values)
-                                             env
-                                             (error "Too much arguments" values))
-                                         env)))))
+                      (set! extend
+                            (lambda (env names values)
+                                 (if (pair? names)
+                                     (if (pair? values)
+                                         ((lambda (newenv)
+                                            (begin
+                                              (set-variable-value! (car names) newenv (car values))
+                                              (extend newenv (cdr names) (cdr values))))
+                                          (enrich env (car names)))
+                                         (error "Too few arguments" names))
+                                     (if (symbol? names)
+                                         ((lambda (newenv)
+                                            (begin
+                                              (set-variable-value! names newenv values)
+                                              newenv))
+                                          (enrich env names))
+                                         (if (null? names)
+                                             (if (null? values)
+                                                 env
+                                                 (error "Too much arguments" values))
+                                             env)))))
                       (set! error (lambda (msg hint)
                                     (exit (list msg hint))))
                       (set! toplevel
@@ -45,35 +45,36 @@
                                       (eval e global-env)))
                                 (read)))
                               (toplevel global-env)))
-                      '(set! eval
-                             (lambda (e r)
-                               (if (pair? e)
-                                   ((lambda (f)
-                                      (if (flambda? f)
-                                          (flambda-apply f r (cdr e))
-                                          (apply f (evlis (cdr e) r))))
-                                    (eval (car e) r))
-                                   (if (symbol? e) (reference e r) e))))
-                      '(set! evlis
-                             (lambda (e* r)
-                               (if (pair? e*)
-                                   ((lambda (v)
-                                      (cons v (evlis (cdr e*) r)))
-                                    (eval (car e*) r))
-                                   '())))
-                      '(set! eprogn
-                             (lambda (e+ r)
-                               (if (pair? (cdr e+))
-                                   (begin (eval (car e+) r)
-                                          (eprogn (cdr e+) r))
-                                   (eval (car e+) r))))
-                      '(set! reference
-                             (lambda (name r)
-                               (if (variable-defined? name r)
-                                   (variable-value name r)
-                                   (if (variable-defined? name global-env)
-                                       (variable-value name global-env)
-                                       (error "No such variable" name)))))
+                      (set! eval
+                            (lambda (e r)
+                              (display "eval") (newline)
+                              (if (pair? e)
+                                  ((lambda (f)
+                                     (if (flambda? f)
+                                         (flambda-apply f r (cdr e))
+                                         (apply f (evlis (cdr e) r))))
+                                   (eval (car e) r))
+                                  (if (symbol? e) (reference e r) e))))
+                      (set! evlis
+                            (lambda (e* r)
+                              (if (pair? e*)
+                                  ((lambda (v)
+                                     (cons v (evlis (cdr e*) r)))
+                                   (eval (car e*) r))
+                                  '())))
+                      (set! eprogn
+                            (lambda (e+ r)
+                              (if (pair? (cdr e+))
+                                  (begin (eval (car e+) r)
+                                         (eprogn (cdr e+) r))
+                                  (eval (car e+) r))))
+                      (set! reference
+                            (lambda (name r)
+                              (if (variable-defined? name r)
+                                  (variable-value name r)
+                                  (if (variable-defined? name global-env)
+                                      (variable-value name global-env)
+                                      (error "No such variable" name)))))
                       ((lambda (quote if set! lambda flambda monitor)
                          (toplevel (the-environment)))
                        (make-flambda  ; quote
@@ -89,7 +90,7 @@
                                   (if (variable-defined? name global-env)
                                       (set-variable-value! name global-env v)
                                       (error "No such variable" name))))
-                            '(eval form r))))
+                            (eval form r))))
                        (make-flambda  ; lambda
                          (lambda (r variables . body)
                            (lambda values
@@ -109,7 +110,7 @@
                    'toplevel 'eval 'evlis 'eprogn 'reference))))))
     (make-toplevel "?? " "== "))
   'make-toplevel
-  ((lambda flambda-tag
+  ((lambda (flambda-tag)
      (list (lambda (behavior) (cons flambda-tag behavior))
            (lambda (o) (if (pair? o) (= (car o) flambda-tag) #f))
            (lambda (f r parms) (apply (cdr f) r parms))))
