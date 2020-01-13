afbce9117528567e2645ac0fb64f31c89b4b5158
Martin Billinger <mbillingr@users.noreply.github.com>
2020-01-13 13:44:46 +0100

    fix race condition in symbol interner

--- project/src/symbol.rs
+++ project/src/symbol.rs
@@ -7,54 +7,49 @@ lazy_static! {
     static ref INTERNED_STRINGS: RwLock<HashSet<&'static str>> = RwLock::new(HashSet::new());
 }
 
-#[derive(Copy, Clone, Eq, PartialEq, Hash)]
+#[derive(Copy, Clone, Hash)]
 pub struct Symbol(&'static str);
 
 impl Symbol {
     pub fn new(s: &'static str) -> Self {
-        if let Some(is) = INTERNED_STRINGS
-            .read()
-            .expect("could not get read lock on interned strings")
-            .get(s)
-        {
-            return Symbol(is);
-        }
-        INTERNED_STRINGS
-            .write()
-            .expect("could not get write lock on interned strings")
-            .insert(s);
-        Symbol(s)
+        Self::interned(s, |s| s)
     }
 
     pub fn from_string(s: String) -> Self {
-        if let Some(is) = INTERNED_STRINGS
-            .read()
-            .expect("could not get read lock on interned strings")
-            .get(s.as_str())
-        {
-            return Symbol(is);
-        }
-        let s = Box::leak(Box::new(s));
-        INTERNED_STRINGS
-            .write()
-            .expect("could not get write lock on interned strings")
-            .insert(s);
-        Symbol(s)
+        Self::interned(s, |s| Box::leak(Box::new(s)))
     }
 
     pub fn from_boxed_str(s: Box<str>) -> Self {
+        Self::interned(s, |s| Box::leak(s))
+    }
+
+    fn interned<T: AsRef<str>>(s: T, into_static: impl Fn(T) -> &'static str) -> Self {
         if let Some(is) = INTERNED_STRINGS
             .read()
             .expect("could not get read lock on interned strings")
-            .get(&*s)
+            .get(s.as_ref())
         {
             return Symbol(is);
         }
-        let s = Box::leak(s);
-        INTERNED_STRINGS
+        let mut lock = INTERNED_STRINGS
             .write()
-            .expect("could not get write lock on interned strings")
-            .insert(s);
+            .expect("could not get write lock on interned strings");
+        if let Some(is) = lock.get(s.as_ref()) {
+            // a different thread may have inserted the symbol while we were waiting for the lock...
+            return Symbol(is);
+        } else {
+            let s = into_static(s);
+            lock.insert(s);
+            Symbol(s)
+        }
+    }
+
+    pub fn uninterned(s: &'static str) -> Self {
+        Symbol(s)
+    }
+
+    pub fn as_uninterned(&self) -> Self {
+        let s = Box::leak(Box::new(self.0.to_owned()));
         Symbol(s)
     }
 
@@ -62,6 +57,14 @@ impl Symbol {
         self.0 as *const str == other.0 as *const str
     }
 }
+
+impl PartialEq for Symbol {
+    fn eq(&self, other: &Self) -> bool {
+        self.ptr_eq(other)
+    }
+}
+
+impl Eq for Symbol {}
 
 impl From<&str> for Symbol {
     fn from(s: &str) -> Symbol {
